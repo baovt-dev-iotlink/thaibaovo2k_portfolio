e841f0ce1fa75d77f8a459685a3efc04a62d3a62
thaibaovo2k <thaibaovo2k@gmail.com>
2022-07-13 15:24:04 +0700

    fix responesive 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,11 +11,11 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <Col size={12} sm={6} className="footer-logo">
+            <img src={logo} alt="Logo" className="footer-logo-img"/>
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
+          <Col size={12} sm={6} className="text-center text-sm-end mb-100vw">
+            <div className="social-icon mb-100vw">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
